e127f0ca8009eeadf7c10c5b7602f6d50e5ea31b
Santiago Moreno <carloss.moreno@gmail.com>
2024-07-10 17:01:59 -0300

    feat(gmmit): Extract Gemini API calls to separate package (#5)
    This commit introduces a new package, `gemini`, which encapsulates all interactions with the Gemini API.
    
    This change aims to improve the organization and maintainability of the codebase by separating concerns. Other changes include:
    - Moving the `RunPrompt`, `SendMessageToModel`, and `ResponseToString` functions to the `gemini` package.
    - Updating the `main` function to use the new `gemini` package.
    - Minor code cleanup and formatting adjustments.

--- cmd/gmmit/main.go
+++ cmd/gmmit/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"os"
 	"fmt"
 	"strings"
 	"log"
 
 	"github.com/google/generative-ai-go/genai"
-	"google.golang.org/api/option"
 
 	. "gitlab.com/orion-rep/gmmit/internal/pkg/common"
+	. "gitlab.com/orion-rep/gmmit/internal/pkg/ai"
 )
 
 var commitStandard, prompt string = "",""
@@ -23,34 +22,17 @@ func main() {
 	
 	Info("Generating commit message.")
 
-    //commitStandard := "Conventional Commits"
     commitStandard = GetEnvArg("GMMIT_COMMIT_PATTERN", "<type>[optional scope]: <description> (#<ticket-id>)")
 	prompt = fmt.Sprintf("Create a git commit message following the \"Conventional Commits\" standard: \"%s\". The Ticket ID MUST be present on the first line, look for it on the branch name: \"%s\". Respond with the commit message only. First line can not be a generic line, must be a specific change. If there are many changes, list the rest at the end. These are the file changes to be pushed:\n%s",
 				commitStandard, gitBranch, gitDiff)
 
 	Debug(prompt)
 
-	ctx := context.Background()
-	client, err := genai.NewClient(ctx, option.WithAPIKey(GetEnvArg("GMMIT_API_KEY")))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-	model := client.GenerativeModel("gemini-1.5-pro")
-	cs := model.StartChat()
-
-	send := func(msg string) *genai.GenerateContentResponse {
-		res, err := cs.SendMessage(ctx, genai.Text(msg))
-		if err != nil {
-			log.Fatal(err)
-		}
-		return res
-	}
-
-	res := send(prompt)
-	stringRes := responseToString(res)
+	res := RunPrompt(prompt)
 	printResponse(res)
+
 	Info("Create a commit with this message? [y/N]")
+
 	reader := bufio.NewReader(os.Stdin)
 	confirmation, err := reader.ReadString('\n')
 		if err != nil {
@@ -59,23 +41,12 @@ func main() {
 	confirmation = strings.ToLower(strings.TrimSpace(confirmation))
 	if confirmation == "y" || confirmation == "yes" {
 		Info("Creating Commit...")
-		gitCommit := RunCommand("git","commit","-m",stringRes)
+		gitCommit := RunCommand("git","commit","-m",ResponseToString(res))
     	Info("Git Command Log:")
 		fmt.Println(string(gitCommit))
 	} 
 }
 
-func responseToString(resp *genai.GenerateContentResponse)(string){
-	stringResponse := ""
-	for _, cand := range resp.Candidates {
-		if cand.Content != nil {
-			for _, part := range cand.Content.Parts {
-				stringResponse += fmt.Sprintln(part)
-			}
-		}
-	}
-	return stringResponse
-}
 func printResponse(resp *genai.GenerateContentResponse) {
 	Info("Generated Text:")
 	Info("---")
---
+++ internal/pkg/ai/gemini.go
@@ -0,0 +1,48 @@
+package gemini
+
+import (
+	"context"
+	"fmt"
+	"log"
+	
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+
+	. "gitlab.com/orion-rep/gmmit/internal/pkg/common"
+)
+
+func RunPrompt(prompt string) (*genai.GenerateContentResponse) {
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(GetEnvArg("GMMIT_API_KEY")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	model := client.GenerativeModel(GetEnvArg("GMMIT_MODEL", "gemini-1.5-pro"))
+	
+	res := SendMessageToModel(ctx, model, prompt)
+
+	return res
+}
+
+func SendMessageToModel(ctx context.Context, model *genai.GenerativeModel, msg string) *genai.GenerateContentResponse {
+	cs := model.StartChat()
+	res, err := cs.SendMessage(ctx, genai.Text(msg))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return res
+}
+
+func ResponseToString(resp *genai.GenerateContentResponse)(string){
+	stringResponse := ""
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				stringResponse += fmt.Sprintln(part)
+			}
+		}
+	}
+	return stringResponse
+}
