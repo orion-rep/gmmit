f13d56ca6071c32ad5279949ad9eeecddd259c14
Santiago Moreno <carloss.moreno@gmail.com>
2024-07-17 17:36:04 -0300

    feat(common): Handle undefined env vars by asking the user (#23)
    This commit introduces a change in behavior when an environment variable is not defined:
    
    - The application now prompts the user to define the missing variable.
    - If the user chooses to define the variable, it's stored in the local environment and the .gmenv file.
    - If the user chooses not to define the variable, the application exits with an error.
    
    Additionally:
    
    - Updated the `Info` function to provide more context about its intended use.
    - Introduced a new `Question` function for displaying interactive prompts to the user.

--- internal/pkg/common/logging.go
+++ internal/pkg/common/logging.go
@@ -15,7 +15,10 @@ func Debug(format string, args ...interface{}) {
 func Info(format string, args ...interface{}) {
 	fmt.Printf("\x1b[34;1m[INFO] %s\x1b[0m\n", fmt.Sprintf(format, args...))
 }
-
+// Info should be used to describe the example commands that are about to run.
+func Question(format string, args ...interface{}) {
+	fmt.Printf("\x1b[36;1m[INFO] %s\x1b[0m ", fmt.Sprintf(format, args...))
+}
 // Warning should be used to display a warning
 func Warning(format string, args ...interface{}) {
 	fmt.Printf("\x1b[33;1m[WARN] %s\x1b[0m\n", fmt.Sprintf(format, args...))
